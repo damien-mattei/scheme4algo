b859032c338490fea21b5579709b9614aca72dd4
Damien MATTEI <damien.mattei@gmail.com>
2020-06-08 00:23:16 +0200

    approx solutions
    approx sol

--- SssRec.scm
+++ SssRec.scm
@@ -14,28 +14,46 @@
 
 (include "../library-FunctProg/first-and-rest.scm")
 
-(define L-init '(1 3 4 16 17 64 256 275 723 889 1040 1041 1093 1111 1284 1344 1520 2027 2734 3000 4285 5027))
+;; if data are disordered the algo works also
+;;(define L-init '(1 3 4 16 17 64 256 275 723 889 1040 1041 1093 1111 1284 1344 1520 2027 2734 3000 4285 5027))
+(define L-init '(1 3 4 16 17 24 45 64 197 256 275 323 540 723 889 915 1040 1041 1093 1099 1111 1284 1344 1520 2027 2500 2734 3000 3267 3610 4285 5027))
 
 ;;(define t-init 19836)
 
+(define t-init 35267)
+  
+(define cpt 0)
+
+;; scheme@(guile-user)> (define t-init (apply + L-init))
+;; scheme@(guile-user)> (ssigma L-init t-init)
+;; $4 = #t
+
+;; scheme@(guile-user)> cpt
+;; $5 = 0
+;; scheme@(guile-user)> (ssigma L-init t-init)
+;; $6 = #t
+;; scheme@(guile-user)> cpt
+;; $7 = 133867
 (define (ssigma L t)
-  ;; (display L)
-  ;; (newline)
-  ;; (display t)
-  ;; (newline)
-  ;; (newline)
+
+  (set! cpt {cpt + 1})
+  ;;(display L) (display " ") (display t) (newline)
   (if (null? L)
-      #f
+      (begin
+	;; (display "null L")
+	;; (newline)
+	;; (newline)
+	#f)
       (let [ (c (first L))
 	     (R (rest L)) ]
 	(cond [ {c = t} #t ] ;; c is the solution
-	      [ {c > t} (ssigma R t) ]
+	      [ {c > t} (ssigma R t) ] ;; c is to big to be a solution but can be an approximation
 	      ;; c < t at this point
 	      ;; c is part of the solution or his approximation
-	      ;; or c is not part of solution
+	      ;; or c is not part of solution or his approximation
 	      [ else {(ssigma R {t - c}) or (ssigma R t)} ] ))))
 
-
+	      ;;[ else (or (ssigma R {t - c}) (ssigma R t)) ] ))))
 
 ;; (ssigma-sol L-init 603 '())
 ;; (275 256 64 4 3 1)
@@ -47,15 +65,35 @@
 ;; (ssigma-sol L-init t-init '())
 ;; (5027 4285 3000 1520 1344 1284 1041 1040 723 275 256 17 16 4 3 1)
 
+;; scheme@(guile-user)> (ssigma-sol L-init t-init '())
+;; $5 = (5027 4285 3000 2734 2027 1520 1344 1284 1111 1093 1041 1040 889 723 275 256 64 17 16 4 3 1)
+;; scheme@(guile-user)> (define sol (ssigma-sol L-init t-init '()))
+;; scheme@(guile-user)> (eq? sol L-init)
+;; $6 = #f
+;; scheme@(guile-user)> (equal? sol L-init)
+;; $7 = #f
+;; scheme@(guile-user)> (equal? sol (reverse L-init))
+;; $8 = #t
+
+;; scheme@(guile-user)> (apply + L-init)
+;; $2 = 40274
+;; scheme@(guile-user)> (ssigma-sol L-init (apply + L-init) '())
+;; $3 = (5027 4285 3610 3267 3000 2734 2500 2027 1520 1344 1284 1111 1099 1093 1041 1040 915 889 723 540 323 275 256 197 64 45 24 17 16 4 3 1)
+;; scheme@(guile-user)> $3
+;; $4 = (5027 4285 3610 3267 3000 2734 2500 2027 1520 1344 1284 1111 1099 1093 1041 1040 915 889 723 540 323 275 256 197 64 45 24 17 16 4 3 1)
+;; scheme@(guile-user)> (apply + $3)
+;; $5 = 40274
+
 
 (define (ssigma-sol L t S)
 
+  (set! cpt {cpt + 1})
   (if (null? L)
       (begin
-	(display "null L")
-	(newline)
-	(display S)
-	(newline)
+	;; (display "null L")
+	;; (newline)
+	;; (display S)
+	;; (newline)
 	#f)
       (let [ (c (first L))
 	     (R (rest L)) ]
